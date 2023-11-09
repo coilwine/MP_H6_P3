82561e0eac983972b9cdfaa2e48262cb7792a160
coilwine <ionicproject0@gmail.com>
2023-11-09 12:11:34 +0300

    Hafta 6 Proje 3

---
+++ app/src/main/java/com/mobilprogramlama/hafta6_proje3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mobilprogramlama.hafta6_proje3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
